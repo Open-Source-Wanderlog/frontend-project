ab28ca4a573381c07ce10464eb76c72073d121eb
CesarrAnn <129460134+CesarrAnn@users.noreply.github.com>
2024-06-06 22:31:12 -0500

    docs: Added Homepages

---
+++ src/app/wanderlog/pages/homepages/body-content/body-content.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-body-content',
+  templateUrl: './body-content.component.html',
+  styleUrl: './body-content.component.css'
+})
+export class BodyContentComponent {
+
+}
